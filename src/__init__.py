e95fdc087cda55e092ed46fdfbe00be8ee7edfda
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-10 17:26:05 +0530

    initializing pygame window with title

--- src/__init__.py
+++ src/__init__.py
@@ -13,3 +13,5 @@ TTT = [[None] * 3, [None] * 3, [None] * 3]
 pg.init()
 fps = 30
 CLOCK = pg.time.Clock()
+screen = pg.display.set_mode((width, height + 100), 0, 32)
+pg.display.set_caption("Tic Tac Toe")
