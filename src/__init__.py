19c78d8e3335026c6129f652efe2935fbb25a789
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-10 17:36:04 +0530

    check for winning column

--- src/__init__.py
+++ src/__init__.py
@@ -70,3 +70,12 @@ def check_win():
             pg.draw.line(screen, (250, 0, 0), (0, (row + 1) * height / 3 - height / 6),
                          (width, (row + 1) * height / 3 - height / 6), 4)
             break
+
+    for col in range(0, 3):
+        if (TTT[0][col] == TTT[1][col] == TTT[2][col]) and (TTT[0][col] is not None):
+            # this column won
+            winner = TTT[0][col]
+            # draw winning line
+            pg.draw.line(screen, (250, 0, 0), ((col + 1) * width / 3 - width / 6, 0),
+                         ((col + 1) * width / 3 - width / 6, height), 4)
+            break
