fd900f7c2082a68a2e26b7bee100beb776ab1b42
sainiprathmesh <prathmesh.saini_cs18@gla.ac.in>
2020-04-10 17:39:17 +0530

    mouse click response recorded

--- src/__init__.py
+++ src/__init__.py
@@ -107,10 +107,43 @@ def drawXO(row, col):
     if col == 3:
         posy = height / 3 * 2 + 30
     TTT[row - 1][col - 1] = XO
-    if (XO == 'x'):
+    if XO == 'x':
         screen.blit(x_img, (posy, posx))
         XO = 'o'
     else:
         screen.blit(o_img, (posy, posx))
         XO = 'x'
     pg.display.update()
+
+
+def userClick():
+    # get coordinates of mouse click
+    x, y = pg.mouse.get_pos()
+
+    # get column of mouse click (1-3)
+    if x < width / 3:
+        col = 1
+    elif x < width / 3 * 2:
+        col = 2
+    elif x < width:
+        col = 3
+    else:
+        col = None
+
+    # get row of mouse click (1-3)
+    if y < height / 3:
+        row = 1
+    elif y < height / 3 * 2:
+        row = 2
+    elif y < height:
+        row = 3
+    else:
+        row = None
+    # print(row,col)
+
+    if row and col and TTT[row - 1][col - 1] is None:
+        global XO
+
+        # draw the x or o on screen
+        drawXO(row, col)
+        check_win()
